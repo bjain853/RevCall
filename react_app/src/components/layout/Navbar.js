302455d2de661e1206cd5666430328a6c8f79527
bjain853 <jainbhavya375@gmail.com>
2020-05-07 10:35:45 +0530

    changes navbar.js

--- react_app/src/components/layout/Navbar.js
+++ react_app/src/components/layout/Navbar.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom'
 import SignedInLinks from './SingedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 
+const color='red darken-3';
+
 const Navbar = () => {
     return (
-        <nav className="nav-wrapper red darken-3">
+        <nav className={"nav-wrapper"+color}>
             <div className="container">
                 <div className="left">
-                    <Link to='/' className='brand-logo'>RevCall</Link>
+                    <Link to='/' className='brand-logo left'>RevCall</Link>
                 </div>
                 <SignedInLinks/>
                 <SignedOutLinks/>
